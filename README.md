2da8e8879eff120f4f5f4482b33d7e5d97c9e8ad
Daniel McClanahan <dmcclanahan@twitter.com>
2016-05-19 23:42:45 -0700

    good first attempt at adding everything

--- README.md
+++ README.md
@@ -19,22 +19,40 @@ Find files fast, using helm. It'll be cool, trust me.
     - `find . \( -name "*a*" \) -and \( -name "*b*" \)` for `helm-pattern` = `a b`
 - bust out keybindings for set intersections and then running commands
 - these keybindings interpret the current pattern as an "and" pattern as shown above and then clear `helm-pattern` to eventually build a `find` command line
-    - <kbd>M-u</kbd> = union
-    - <kbd>M-i</kbd> = intersection
-    - <kbd>M-c</kbd> = complement (current)
-    - <kbd>M-r</kbd> = regex mode
-    - <kbd>M-a</kbd> = "arg" mode (raw find arguments)
-    - <kbd>M-(</kbd> = open paren (will implicitly close any left at end)
-    - <kbd>M-)</kbd> = close paren
-    - <kbd>M-g</kbd> = run command on results in new `*Async Shell Command*` buffer and close helm
-    - <kbd>M-l</kbd> = list the files in a dired buffer and exit
-    - <kbd>M-f</kbd> = prompt for filetype (`b|c|d|f|l|p|s`)
-    - <kbd>M-p</kbd> = start search from project root
-    - <kbd>M-d</kbd> = start search from initial choice of  `default-directory`
-    - <kbd>M-f</kbd> = choose directory to search from, starting at whatever the current choice is
-    - <kbd>M-j</kbd> = start searching a directory up
-    - <kbd>M-z</kbd> = undo whatever was just done
-- otherwise, pressing <kbd>RET</kbd> on a result visits it, and <kbd>TAB</kbd> previews it
+    - combinators
+        - <kbd>M-o</kbd> = union ("or")
+        - <kbd>M-a</kbd> = intersection ("and")
+    - groupings
+        - <kbd>M-(</kbd> = open paren (will implicitly close any left at end)
+        - <kbd>M-)</kbd> = close paren
+    - modes
+        - <kbd>M-r</kbd> = regex mode
+        - <kbd>M-n</kbd> = normal text mode (not regex mode)
+        - <kbd>M-f</kbd> = "find" mode (just input raw find arguments)
+        - <kbd>M-f</kbd> = filetype (`b|c|d|f|l|p|s`)
+            - explode if not recognized
+    - modifiers
+        - <kbd>M-c</kbd> = toggle complement (current)
+    - actions
+        - <kbd>M-g</kbd> = run command on results in new `*Async Shell Command*` buffer and close helm
+        - <kbd>M-l</kbd> = list the files in a dired buffer and exit
+        - <kbd>RET</kbd> = visit
+        - <kbd>TAB</kbd> = preview
+    - meta
+        - <kbd>M-z</kbd> = undo whatever was just done
+        - <kbd>C-M-z</kbd> = redo
+        - <kbd>M-m</kbd> = set `mindepth`
+        - <kbd>M-d</kbd> = set `maxdepth`
+
+## other helm things in general
+
+add these to all helm searches
+
+- <kbd>M-p</kbd> = start search from project root
+- <kbd>M-i</kbd> = start search from initial choice of `default-directory`
+- <kbd>M-f</kbd> = choose directory to search from, starting at whatever the current choice is
+- <kbd>M-j</kbd> = start searching a directory up
+
 
 # License
 
--- f3.el
+++ f3.el
@@ -1,25 +1,147 @@
-;;; make an async source of sorts such that when the query is changed:
-;;; 1. the current set of results which match the new query are fed into the
-;;; newly-spawned find process so that they're not duplicated, and are already
-;;; available to search for the next query, and
-;;; 2. if results in the previous query match the new query, they are kept in
-;;; the buffer (thanks to 1., i think), and actions (such as calling find-file)
-;;; can be performed on them
+;;; f3.el --- helm interface for searching files really fast
 
-;;; consider writing find replacement which can run faster, with a more limited
-;;; set of options. it's unlikely this will work since find is in c and i don't
-;;; want to have to package and maintain a c executable over a million different
-;;; distributions, but it is possible
+;; Keywords: find, files, helm
 
-;;; make the pattern, instead of just a regexp, allow for different find options
+;;; Commentary:
 
-;;; think about cool actions you can take on the input. previewing? getting file
-;;; stats? running an executable? what options does helm have to union /
-;;; intersection / complement search results? can we integrate icicles?
+;;; Code:
 
-;;; when this works, make the source creation part into an external library, and
-;;; then depend on that
+(require 'cl-lib)
+(require 'helm)
 
-;;; don't forget to read the find man page about optimization levels
+;;; functions to parse patterns into an AST
+;;; TODO: defcustom for default input mode, and persist across combinators
+(defconst f3-input-modes
+  '((text . f3-create-text-pattern)
+    (regex . f3-create-regex-pattern)
+    (find . f3-create-find-pattern)
+    (filetype . f3-create-filetype-pattern))
+  "Modes which interpret the current `helm-pattern' differently.")
+
+(defun f3-create-text-pattern (pat)
+  (let ((texts (helm-mm-split-pattern pat)))
+    (cl-reduce
+     (lambda (parsed1 parsed2) (list :and parsed1 parsed2))
+     (cl-mapcar (lambda (pat) (list :text pat)) texts))))
+
+(defun f3-create-regex-pattern (pat)
+  (let ((texts (helm-mm-split-pattern pat)))
+    (cl-reduce
+     (lambda (parsed1 parsed2) (list :and parsed1 parsed2))
+     (cl-mapcar (lambda (pat) (list :regex pat)) texts))))
+
+(defun f3-create-find-pattern (pat)
+  "Assumes correctness of pattern PAT."
+  (list :raw pat))
+
+(defconst f3-valid-filetype-patterns '("b" "c" "d" "f" "l" "p" "s")
+  "Valid filetype arguments to unix find.")
+
+(defun f3-create-filetype-pattern (pat)
+  (unless (member pat f3-valid-filetype-patterns)
+    (error (format "%s %s" "invalid filetype pattern" pat)))
+  (list :filetype pat))
+
+(defconst f3-combinators '(:and :or))
+
+(defun f3-do-complement (ast do-complement)
+  (if do-complement (list :not ast) ast))
+
+(defun f3-pattern-to-parsed-arg (pattern mode complement)
+  (let ((process-input-fn (assoc mode f3-input-modes)))
+    (if process-input-fn
+        (f3-do-complement (funcall process-input-fn pattern) complement)
+      (error (format "%s %S" "invalid mode" mode)))))
+
+(defun f3-maybe-lowercase-generate (base pat)
+  (let ((cleaned-pat (replace-regexp-in-string "\"" "" pat)))
+    ;; we generate base, so no injection
+    (if (string-match-p "[[:upper:]]" cleaned-pat)
+        (format "-%s \"%s\"" base cleaned-pat)
+      (format "-i%s \"%s\"" base cleaned-pat))))
+
+(defun f3-parsed-to-command (parsed-args)
+  "Transform PARSED-ARGS to a raw find command."
+  (pcase parsed-args
+    ;; operators
+    (`(:and . ,args)
+     (cl-reduce (lambda (str1 str2) (format "\\( %s \\) -and \\( %s \\)"
+                                            str1 str2))
+                (cl-mapcar #'f3-parsed-to-command args)))
+    (`(:or . ,args)
+     (cl-reduce (lambda (str1 str2) (format "\\( %s \\) -or \\( %s \\)"
+                                            str1 str2))
+                (cl-mapcar #'f3-parsed-to-command args)))
+    (`(:not ,thing) (format "-not \\( %s \\)") (f3-parsed-to-command thing))
+    ;; modes
+    (`(:text ,thing) (f3-maybe-lowercase-generate "name" thing))
+    (`(:regex ,thing) (f3-maybe-lowercase-generate "regex" thing))
+    (`(:raw ,thing) thing)
+    (`(:filetype ,thing) (format "-type %s" thing))
+    (_  parsed-args)))
+
+;;; TODO: defcustom this
+(defconst f3-default-combinator :and)
+(defvar-local f3-current-combinator f3-default-combinator)
+;;; TODO: defcustom this
+(defconst f3-default-mode :text)
+(defvar-local f3-current-mode f3-default-mode)
+
+;;; TODO: restart `helm-pattern' with whatever was in it before `f3-open-paren'
+;;; or `f3-close-paren' was called, after calling it
+(defun f3-open-paren (dir cur-cmd paren-stack)
+  (f3-do dir nil (cons (list f3-current-combinator cur-cmd) paren-stack)
+         f3-current-combinator f3-current-mode))
+
+
+(defun f3-close-paren (dir cur-cmd paren-stack)
+  (let ((cur-paren (car paren-stack))
+        (upper (cdr paren-stack)))
+    (cl-destructuring-bind (comb cmd) cur-paren
+      (f3-do dir (list comb cmd cur-cmd) upper f3-current-combinator
+             f3-current-mode))))
+
+(defun f3-close-parens-to-make-command (cur-cmd paren-stack)
+  (cl-reduce
+   (lambda (cur stack-el)
+     (cl-destructuring-bind (comb cmd) stack-el (list comb cur cmd)))
+   paren-stack
+   :initial-value cur-cmd))
+
+;;; TODO: defcustom this
+(defconst f3-default-complement nil)
+(defvar-local f3-current-complement f3-default-complement)
+
+;;; TODO: defcustom this
+(defconst f3-find-program "find")
+
+(defun f3-do (dir cur-cmd paren-stack)
+  "CUR-CMD is a list of parsed arguments, not plain text."
+  ;; turn current pattern into something for find, if non-blank
+  (let* ((current-pattern
+          (unless (string-empty-p helm-pattern)
+            (f3-pattern-to-parsed-arg
+             helm-pattern f3-current-mode f3-current-complement)))
+         (combined-pattern (list f3-current-combinator cur-cmd current-pattern))
+         (complete-pattern
+          (f3-close-parens-to-make-command combined-pattern paren-stack))
+         (find-cmd
+          (format "%s \"%s\" %s"
+                  f3-find-program dir (f3-parsed-to-command complete-pattern))))
+    ;; TODO: run helm with find as an async source!
+    ))
+
+;;; TODO: make defcustom for f3's default directory
+
+;;;###autoload
+(defun f3 (start-dir)
+  (interactive (list default-directory))
+  ;; in case user modifies defcustoms after this file is loaded
+  (setq-local
+   f3-current-combinator f3-default-combinator
+   f3-current-mode f3-default-mode
+   f3-current-complement f3-default-complement)
+  (f3-do ))
 
 (provide 'f3)
+;;; f3.el ends here
